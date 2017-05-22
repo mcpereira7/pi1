3e86a105c4ee552d5dd65e867dad6e8ff28bd0ce
Ultrabook HP <Ultrabook HP@192.168.33.116>
2017-05-22 20:40:03 -0300

    criação do objeto power

---
+++ src/entity/TypeWeakeness.java
@@ -0,0 +1,57 @@
+package entity;
+
+public class TypeWeakeness {
+
+    private int id;
+    private int typeAtk;
+    private int typeDef;
+    private int power;
+
+    public TypeWeakeness() {
+   
+    }
+
+    public TypeWeakeness(int id, int typeAtk, int typeDef, int power) {
+        this.id = id;
+        this.typeAtk = typeAtk;
+        this.typeDef = typeDef;
+        this.power = power;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTypeAtk() {
+        return typeAtk;
+    }
+
+    public void setTypeAtk(int typeAtk) {
+        this.typeAtk = typeAtk;
+    }
+
+    public int getTypeDef() {
+        return typeDef;
+    }
+
+    public void setTypeDef(int typeDef) {
+        this.typeDef = typeDef;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+    
+    
+
+}
