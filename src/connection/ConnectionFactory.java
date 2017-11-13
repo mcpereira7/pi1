122c6b6e4aec8a3c32be6254de9a5f7a5a75e234
Leandrowisky <leandro10le@hotmail.com>
2017-11-13 17:41:05 -0200

    Update ConnectionFactory.java

--- src/connection/ConnectionFactory.java
+++ src/connection/ConnectionFactory.java
@@ -13,7 +13,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/pokemonbattle";
     private static final String USER = "root";
-    private static final String PASS = "rom08_01";
+    private static final String PASS = "";
 
     public static Connection getConnection() {
         try {
